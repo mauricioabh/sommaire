069200b74cc1cc1a6f4e12444eb07ce260900c47
Mauricio Barragan <mauricioabh@gmail.com>
2025-09-05 12:07:55 -0600

    fix origin_url test1

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -2,4 +2,4 @@ export const isDev = process.env.NODE_ENV === "development";
 
 export const ORIGIN_URL = isDev
   ? "http://localhost:3000"
-  : "sommaire-iota.vercel.app";
+  : "https://sommaire-iota.vercel.app";
